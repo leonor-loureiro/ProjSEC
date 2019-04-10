adae60cea3964e82c28bd4f86744e25fd2461c3c
Anfifo <afonseca-1995@hotmail.com>
2019-04-11 00:51:25 +0100

    soft shutdown to server added

--- Communication/src/main/java/communication/RequestsReceiverRunnable.java
+++ Communication/src/main/java/communication/RequestsReceiverRunnable.java
@@ -9,16 +9,26 @@ import java.io.IOException;
  */
 public class RequestsReceiverRunnable implements Runnable {
 
-    RequestsReceiver receiver;
-    int port;
-    IMessageProcess processor;
+    private RequestsReceiver receiver;
+    private int port;
+    private IMessageProcess processor;
 
+    /**
+     * Attributes that are required for request reception
+     * @param receiver the request receiver class
+     * @param port port in which the requests will be received
+     * @param processor the processor that will process the messages
+     */
     RequestsReceiverRunnable(RequestsReceiver receiver, int port, IMessageProcess processor){
         this.receiver = receiver;
         this.port = port;
         this.processor = processor;
     }
 
+    /**
+     * initializes the server request reception in a new thread
+     * each message is processed with the object attributed to processor attribute
+     */
     public void run() {
         try {
             receiver.initialize(port, processor);
